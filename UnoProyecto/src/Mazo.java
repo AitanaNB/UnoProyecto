8409ccf22198bf7b83b5438a8f4cb94ae8f85ccd
AitanaNB <165592137+AitanaNB@users.noreply.github.com>
2024-04-29 18:43:58 +0200

    Update Mazo.java

--- UnoProyecto/src/Mazo.java
+++ UnoProyecto/src/Mazo.java
@@ -1,5 +1,75 @@
 package org.pmoo.proyecto;
 
-public class Mazo {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Mazo //MAE que contiene todas las cartas
+{
+	private static Mazo miMazo= new Mazo();
+	private ArrayList<Carta> lista;
+	
+	private Mazo()
+	{
+		this.lista = new ArrayList<Carta>();
+		
+	}
+	public static Mazo getMazo()
+	{
+		return miMazo;
+	}
+	
+	public void inicializarMazo() //crea la baraja de cartas
+	{
+		int i=0;
+		do { 
+			//añadir las cartas con números
+			CartaNum estaCarta= new CartaNum("Rojo", i);
+			miMazo.lista.add(estaCarta);
+			miMazo.lista.add(estaCarta);
+			//System.out.println("color"+estaCarta.getColor()+"Numero"+estaCarta.getNum());
+			
+			estaCarta= new CartaNum("Amarillo", i);
+			miMazo.lista.add(estaCarta);
+			miMazo.lista.add(estaCarta);
+			
+			estaCarta= new CartaNum("Verde", i);
+			miMazo.lista.add(estaCarta);
+			miMazo.lista.add(estaCarta);
+			
+			estaCarta= new CartaNum("Azul", i);
+			miMazo.lista.add(estaCarta);
+			miMazo.lista.add(estaCarta);
+			
+			i++;			
+		}while (i<10);
+		
+		//añadir las cartas especiales
+		i=0;
+		do {
+			CartaEsp estaEsp= new CartaEsp("Rojo");
+			miMazo.lista.add(estaEsp);
+			
+			estaEsp= new CartaEsp("Amarillo");
+			miMazo.lista.add(estaEsp);
+			
+			estaEsp= new CartaEsp("Verde");
+			miMazo.lista.add(estaEsp);
+			
+			estaEsp= new CartaEsp("Azul");
+			miMazo.lista.add(estaEsp);
+			
+			i++;
+		} while (i<2);		
+	}
+	public void mezcalrCarta()
+	{
+		Collections.shuffle(this.lista);
+	}
+	
+	public Carta quitarCartaDelMazo()
+	{
+		return this.lista.get(this.lista.size()-1);
+	}
+		
 }
+
