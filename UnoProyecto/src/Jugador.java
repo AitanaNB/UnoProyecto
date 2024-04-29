010671866cca82c8844377e0c4dc29dfb16d9bb2
ZynJ27 <165598494+ZynJ27@users.noreply.github.com>
2024-04-29 19:15:25 +0200

    Update Jugador.java

--- UnoProyecto/src/Jugador.java
+++ UnoProyecto/src/Jugador.java
@@ -8,7 +8,7 @@ public class Jugador
 	public Jugador (String pNombre, Baraja pBaraja)
 	{
 		this.nombre=pNombre;
-		this.baraja=new Baraja();
+		this.baraja=pBaraja;
 	}
 	
 	public String getNombre()
@@ -16,9 +16,15 @@ public class Jugador
 		return this.nombre;
 	}
 	
+	public void setNombre(String pNombre) 
+	{
+		this.nombre=pNombre;
+	}
+	
 	public void robarCarta ()
 	{
-		
+		Carta cartaRobada = Mazo.getMazo().quitarCartaDelMazo();
+	    agregarCartaAMano(cartaRobada);
 	}
 	
 	public void gritarUno()
@@ -29,9 +35,39 @@ public class Jugador
 		}
 	}
 	
-	public void usarCarta(int pPos)
+	public void usarCarta(int pos)
 	{
-		
+	    if (pos >= 0 && pos < baraja.contar()) {
+	        Carta carta = baraja.obtenerCarta(pos); //obtiene la carta en la posición pos
+	        if (carta.puedeUsarse()) 
+	        {
+	            // La carta puede usarse, la removemos de la baraja del jugador y la agregamos a la pila de descarte
+	            baraja.quitar(carta);
+	            PilaDescarte.getPilaDescarte().anadirCarta(carta);
+	            System.out.println(nombre + " ha jugado una carta.");
+	        } 
+	        else 
+	        {
+	            System.out.println("No puedes jugar esa carta en este momento.");
+	            // El jugador no puede jugar la carta, roba una del mazo
+	            Carta cartaRobada = Mazo.getMazo().quitarCartaDelMazo();
+	            if (cartaRobada.puedeUsarse()) {
+	                // Si la carta robada se puede usar, el jugador la juega inmediatamente
+	                PilaDescarte.getPilaDescarte().anadirCarta(cartaRobada);
+	                System.out.println(nombre + " ha robado una carta y la ha jugado.");
+	            } 
+	            else 
+	            {
+	                // Si la carta robada tampoco se puede usar, el jugador roba otra carta y finaliza su turno
+	                cartaRobada = Mazo.getMazo().quitarCartaDelMazo();
+	                System.out.println(nombre + " ha robado una carta pero no puede jugarla.");
+	            }
+	        }
+	    } 
+	    else 
+	    {
+	        System.out.println("Posición de carta inválida.");
+	    }
 	}
 	
 	public void agregarCartaAMano(Carta pCarta)
