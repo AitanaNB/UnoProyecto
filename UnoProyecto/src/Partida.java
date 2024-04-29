da78138b1cee3ff98a069f7736ba5db592b6db23
AitanaNB <165592137+AitanaNB@users.noreply.github.com>
2024-04-29 18:45:20 +0200

    Update Partida.java

--- UnoProyecto/src/Partida.java
+++ UnoProyecto/src/Partida.java
@@ -19,7 +19,26 @@ public class Partida
 	
 	public void jugarPartida()
 	{
+		Mazo.getMazo().inicializarMazo();
+		Mazo.getMazo().mezcalrCarta();
+		System.out.println("Mezclando cartas...");
 		
+		System.out.println("");
+		System.out.println("Introduce nombre del jugador uno y pulsa intro :) :");
+		
+		String nombre1;
+		nombre1=Teclado.getTeclado().leerString();
+		Teclado.getTeclado().leerString();
+		jugador1.setNombre(nombre1);
+		System.out.println("El nombre del jugador uno es: "+nombre1);
+		
+		System.out.println("");
+		System.out.println("Introduce nombre del jugador dos y pulsa intro :) :");
+		
+		String nombre2;
+		nombre2= Teclado.getTeclado().leerString();
+		jugador2.setNombre(nombre2);
+		System.out.println("El nombre del jugador dos es: "+nombre2);
 	}
 	
 	public void repartirCartas(Jugador pJugador)
