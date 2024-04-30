e06446b3556da420dd045353b26acefb94ee2938
ZynJ27 <165598494+ZynJ27@users.noreply.github.com>
2024-04-30 12:10:04 +0200

    Update PilaDescarte.java

--- UnoProyecto/src/PilaDescarte.java
+++ UnoProyecto/src/PilaDescarte.java
@@ -25,15 +25,6 @@ public class PilaDescarte
 	{
 		return this.lista.get(this.lista.size()-1);
 	}
-	public boolean puedeUsarse(Carta pCarta)
-	{
-		boolean siPuede=false;
-		if(this.obtenerUltimaCarta().getColor()==pCarta.getColor() || this.obtenerUltimaCarta().getNum()==pCarta.getNum())
-		{
-			siPuede=!siPuede;
-		}
-		return siPuede;
-	}
 
 	public ArrayList<Carta> obtenerCartasSinUltima()
 	{
